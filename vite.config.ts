1b0a7c73f59b3994d5ae99d07ca3f3c5249d30de
Soumen <soumen19j@gmail.com>
2026-05-30 22:55:51 +0530

    for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,8 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    // Configure Nitro for Vercel deployment
+    preset: "vercel",
+  },
 });
